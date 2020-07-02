398c5f4836ecd96006f6ce16126cbdfaaa1da3c6
trololo <solembum@sole.com>
2020-07-02 20:40:33 +0300

    view task list

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Bootstrap шаблон... -->
+
+    <div class="panel-body">
+        <!-- Текущие задачи -->
+        <a href="{{ route('tasks.create') }}" class="btn btn-success"><i class="fa fa-plus"> Новая задача</i></a>
+        @if (count($tasks) > 0)
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Текущие задачи
+                </div>
+
+                <div class="panel-body">
+                    <table class="table table-striped task-table">
+
+                        <!-- Заголовок таблицы -->
+                        <thead>
+                        <tr>
+                            <th>Название</th>
+                            <th>Действие</th>
+                        </thead>
+                        </tr>
+
+                        <!-- Тело таблицы -->
+                        <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <!-- Имя задачи -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <form action="{{ route('tasks.destroy') }}" method="POST">
+                                        {{ method_field('DELETE')}}
+                                        {{ csrf_field() }}
+                                        <button class="btn btn-danger">
+                                            <i class="fa fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @endif
+    </div>
+@endsection
